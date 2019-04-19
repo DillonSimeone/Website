3445f1b5ca3637d76b8771d643f95c25a8ef806c
DillonSimeone <Dillonsimeone@gmail.com>
2019-04-19 05:01:42 -0700

    Nav background color upgrade

--- randomColors.js
+++ randomColors.js
@@ -3,6 +3,7 @@ const colorScheme1 = ["#bee6ce", "#bcffdb", "#8dffcd", "#44bba4", "4f9d69"];
 //From shrio
 const palette = ["#1abc9c", "#2ecc71","#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#ecf0f1", "#95a5a6", "#f39c12", "#d35400", "#c0392b", "#bdc3c7", "#7f8c8d"];
 
+
 function SchemeColors(scheme){
     switch(scheme){
         case 1: 
@@ -61,6 +62,9 @@ function randomRgba(min, max, alpha){
     return "rgba(" + randomColor(min, max) + "," + randomColor(min, max) + "," + randomColor(min,max) + "," + alpha + ")";
 };
 
+function ItemPaletteColors(target){
+    document.querySelector(target).style.background = "linear-gradient(to right, " + randomPaletteColor() + " , " + randomPaletteColor() + " , " + randomPaletteColor() + ")";
+}
 //Add random linear gradient to target element.
 function ItemColors(min, max, alpha, target){
     document.querySelector(target).style.background = "linear-gradient(to right, " + randomRgba(min, max, alpha) + " , " + randomRgba(min, max, alpha) + " , " + randomRgba(min, max, alpha) + ")";
