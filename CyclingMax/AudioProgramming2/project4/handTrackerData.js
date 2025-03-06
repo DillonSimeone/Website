3ab030e6d1560b07d98040097ab0b155096a9983
DillonSimeone <dillonsimeone@gmail.com>
2025-03-05 21:18:06 -0800

    Adding fingertip controls to Project 4 patch

---
+++ CyclingMax/AudioProgramming2/project4/handTrackerData.js
@@ -0,0 +1,18 @@
+const maxApi = require("max-api");
+const WebSocket = require("./node_modules/ws");
+
+// Create WebSocket Server
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        const data = JSON.parse(message);
+        for (const [dial, value] of Object.entries(data)) {
+            maxApi.outlet(dial, value);
+        }
+    });
+
+    ws.send("Connected to Max!");
+});
+
+maxApi.post("WebSocket Server running on port 8080");
