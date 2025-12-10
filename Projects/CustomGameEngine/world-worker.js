7259469dc0a3b1ccf238f284b6ff3847f4d0b221
DillonSimeone <dillonsimeone@gmail.com>
2025-12-10 15:33:20 -0800

    Fix pathing

--- Projects/CustomGameEngine/world-worker.js
+++ Projects/CustomGameEngine/world-worker.js
@@ -1,12 +1,19 @@
 // world-worker.js
 
+let baseUrl = '/'; // Default base URL
+
 self.onmessage = async (e) => {
+    if (e.data.type === 'init') {
+        baseUrl = e.data.baseUrl;
+        return; // Important: do not proceed with loadModel if it's just an init message
+    }
+
     if (e.data.type === 'loadModel') {
         const modelName = e.data.modelName;
         
         try {
             // 1. Fetch Manifest
-            const manifestUrl = `/assets/${modelName}/manifest.json`;
+            const manifestUrl = `${baseUrl}assets/${modelName}/manifest.json`;
             const manifestRes = await fetch(manifestUrl);
             if (!manifestRes.ok) throw new Error(`Could not find manifest for ${modelName}`);
             const manifest = await manifestRes.json();
@@ -14,7 +21,7 @@ self.onmessage = async (e) => {
 
             // 2. Stream Visual Chunks
             for (const chunkFile of manifest.chunkFiles) {
-                const chunkUrl = `/assets/${modelName}/${chunkFile.file}`;
+                const chunkUrl = `${baseUrl}assets/${modelName}/${chunkFile.file}`;
                 const chunkRes = await fetch(chunkUrl);
                 const chunkData = await chunkRes.json();
                 self.postMessage({ type: 'chunkLoaded', chunkData });
@@ -22,7 +29,7 @@ self.onmessage = async (e) => {
             
             // 3. Stream Pre-baked Physics Chunks
             self.postMessage({ type: 'physicsProcessingStart' });
-            const physicsUrl = `/assets/${modelName}/${manifest.physicsFile}`;
+            const physicsUrl = `${baseUrl}assets/${modelName}/${manifest.physicsFile}`;
             const physicsRes = await fetch(physicsUrl);
             const physicsData = await physicsRes.json(); // This is an array of box-groups
 
