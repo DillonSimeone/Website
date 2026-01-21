26b414ee27b2a3cefa83b24af03f603ba7944678
DillonSimeone <dillonsimeone@gmail.com>
2026-01-21 00:32:15 -0800

    Arduino has forsaken us all... All praise to PlatformIO!

---
+++ ESP32Codes/PlatformIO/PWMStatusLight/.pio/build/esp32c3/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32c3/FrameworkArduino/FirmwareMSC.cpp.o: \
+ C:/Users/DoctorNightmares/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ C:/Users/DoctorNightmares/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ C:/Users/DoctorNightmares/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/DoctorNightmares/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/qio_qspi/include/sdkconfig.h
