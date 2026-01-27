d93b8fd9d4227bf727f470c21577d49ae48e9c40
DillonSimeone <dillonsimeone@gmail.com>
2026-01-26 16:48:50 -0800

    Ultra-optimization!

---
+++ RegenerateBackground.bat
@@ -0,0 +1,11 @@
+@echo off
+echo =======================================================
+echo     Regenerating Static Background Assets
+echo =======================================================
+echo.
+python rebuild.py
+echo.
+echo =======================================================
+echo     DONE! Refresh your browser to see the changes.
+echo =======================================================
+pause
